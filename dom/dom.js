ef82cf797393ac98e1c9ad0dff6cc5d041ca3fee
huyiling1111 <1052859577@qq.com>
2020-09-25 22:20:02 +0800

    修改dom封装

--- dom/dom.js
+++ dom/dom.js
@@ -1,6 +1,6 @@
 window.dom = {
-    find(selector,scope) {
-        return (document||scope).querySelectorAll(selector)
+    find(selector, scope) {
+        return (document || scope).querySelectorAll(selector)
     },
     style(node, key, value) {
         node.style[key] = value
@@ -8,7 +8,150 @@ window.dom = {
     },
     each(nodes, fn) {
         for (let i = 0; i < nodes.length; i++) {
-            fn.call(null,nodes[i])
+            fn.call(null, nodes[i])
         }
+    },
+    creat(string) {
+        let container = document.createElement('template')
+        container.innerHTML = string.trim()
+        //用trim()去掉多余的空格
+        return container.content.firstChild
+        //一定要这样写container.content.firstChild
+    },
+    after(node1, node2) {
+        node1.parentNode.insertBefore(node2, node1.nextSibling,)
+        //已有的子节点前插入一个新的子节点
+
+    },
+    before(node1, node2) {
+        node1.parentNode.insertBefore(node2, node1)
+        //已有的子节点前插入一个新的子节点
+    },
+    append(parent, child) {
+        parent.appendChild(child)
+    },
+    swap(child, parent) {
+        dom.after.call(null, child, parent)
+        parent.appendChild(child)
+        return parent
+    },
+    remove(node) {
+        node.parentNode.removeChild(node)
+        return node
+    },
+    empty(parent) {
+        const array = []
+        let x = node.firstChild
+        while (x) {
+            array.push(dom.remove(node.firstChild))
+            x = node.firstChild
+        }
+        return array
+    },
+    attr(node, name, value) {//重载
+        if (arguments.length === 3) {
+            node.setAttribute(name, value)
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name)
+        }
+    },
+
+    text(node, string) { // 适配
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string
+            } else {
+                node.textContent = string
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText
+            } else {
+                return node.textContent
+            }
+        }
+    },
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            // dom.style(div, 'color', 'red')
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                // dom.style(div, 'color')
+                return node.style[name]
+            } else if (name instanceof Object) {
+                // dom.style(div, {color: 'red'})
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key]
+                }
+            }
+        }
+    },
+    class: {
+        add(node, className) {
+            node.classList.add(className)
+        },
+        remove(node, className) {
+            node.classList.remove(className)
+        },
+        has(node, className) {
+            return node.classList.contains(className)
+        }
+    },
+    on(node, eventName, fn) {
+        node.addEventListener(eventName, fn)
+    },
+    off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn)
+    },
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
+    },
+    parent(node) {
+        return node.parentNode
+    },
+    children(node) {
+        return node.children
+    },
+    siblings(node) {
+        return Array.from(node.parentNode.children)
+            .filter(n => n !== node)
+    },
+    next(node) {
+        let x = node.nextSibling
+        while (x && x.nodeType === 3) {
+            x = x.nextSibling
+        }
+        return x
+    },
+    previous(node) {
+        let x = node.previousSibling
+        while (x && x.nodeType === 3) {
+            x = x.previousSibling
+        }
+        return x
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    },
+    index(node) {
+        const list = dom.children(node.parentNode)
+        let i
+        for (i = 0; i < list.length; i++) {
+            if (list[i] === node) {
+                break
+            }
+        }
+        return i
     }
 }
--- dom/main.js
+++ dom/main.js
@@ -3,3 +3,47 @@ dom.style(div, 'color', 'red') // 设置 div.style.color
 
 const divList = dom.find('.red') // 获取多个 div.red 元素
 dom.each(divList, (n) => console.log(n)) // 遍历 divList 里的所有元素
+
+const div1 = dom.creat('<div>哈哈</div>')
+dom.after(test, div1)
+const div2 = dom.creat('<div>儿子</div>')
+dom.append(test, div2)
+
+const div3 = dom.creat('<div class="parent"></div>')
+dom.swap(test, div3)
+dom.empty(div3)
+dom.attr(div3, 'title', 'haha')
+dom.attr(div3, `title:haha`)
+dom.text(div3, '您好，这是新的内容')
+dom.style(test, { border: '1px solid red', color: 'blue' })
+console.log(dom.style(test, 'border'))
+dom.style(test, 'border', '1px solid black')
+
+dom.class.add(test, 'red')
+dom.class.add(test, 'blue')
+dom.class.remove(test, 'blue')
+console.log(dom.class.has(test, 'blue'))
+
+const fn = () => {
+    console.log('点击了')
+}
+dom.on(test, 'click', fn)
+dom.off(test, 'click', fn)
+
+
+const testDiv = dom.find('#test')[0]
+console.log(testDiv)
+const test2 = dom.find('#test2')[0]
+console.log(dom.find('.red', test2)[0])
+
+console.log(dom.parent(test))
+
+const s2 = dom.find('#s2')[0]
+console.log(dom.siblings(s2))
+console.log(dom.next(s2))
+console.log(dom.previous(s2))
+
+const t = dom.find('#travel')[0]
+dom.each(dom.children(t), (n) => dom.style(n, 'color', 'red'))
+
+console.log(dom.index(s2))
